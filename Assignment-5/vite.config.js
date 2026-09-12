73679ff2c664700b8b2b27bacf75af43100e55af
Manders93 <asedgwick2@my.wctc.edu>
2026-09-12 17:04:18 -0500

    trying to fix assignment 5 link. GHpages is throwing 404

--- Assignment-5/vite.config.js
+++ Assignment-5/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
   base: '/AWD-homework/Assignment-5/',
+  plugins: [react()],
 })
