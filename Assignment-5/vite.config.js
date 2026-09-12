28b52d034bb4d51ab9727ceb5a17ac731e2f2e89
Manders93 <asedgwick2@my.wctc.edu>
2026-09-12 16:55:17 -0500

    Update vite config file

--- Assignment-5/vite.config.js
+++ Assignment-5/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/AWD-homework/Assignment-5/',
   plugins: [react()],
+  base: '/AWD-homework/Assignment-5/',
 })
